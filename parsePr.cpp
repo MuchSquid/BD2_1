e374b5741f64d72e07138f7e8d96b87860101462
FlaviaManuico <83974322+FlaviaManuico@users.noreply.github.com>
2024-09-28 18:11:38 -0500

    Archivo prueba SQL

---
+++ parsePr.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <vector>
+#include "parser.hh"
+// #include "ISAM.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    string sentencia;
+    while (true) {
+        cout << "Inserte su sentencia SQL: " << endl;
+        cin.ignore(); 
+        getline(cin, sentencia);
+
+        cout << "Sentencia ingresada: " << sentencia << endl;
+
+        Scanner scanner(sentencia + " ");
+        Parser parser(&scanner);
+        
+        try {
+            parser.parse();
+        } catch (const exception& e) {
+            cout << "Error durante el parseo: " << e.what() << endl;
+        }
+        
+        if (!error_message.empty()) {
+            cout << "Mensaje de error: " << error_message << endl;
+        } else {
+            cout << "Parseo exitoso, sin errores." << endl;
+        }
+    }
+    return 0;
+}
